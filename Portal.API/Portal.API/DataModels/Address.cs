848592de11b20df87642770203400bdca5dbd9f8
tvd <sonthanh123bg@gmail.com>
2022-12-04 00:44:52 +0700

    Initial Commit

---
+++ Portal.API/Portal.API/DataModels/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace Portal.API.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+
+        public string PhysicalAddress { get; set; }
+
+        public string PostalAddress { get; set; }
+
+        // Navigation Properties
+        public Guid StudentId { get; set; }
+    }
+}
+
---
+++ Portal.API/Portal.API/DataModels/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace Portal.API.DataModels
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
